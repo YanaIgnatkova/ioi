0e1286107d0d2df8e2c3c16a11bc2a758aa819dc
YanaIgnatkova <43076614+YanaIgnatkova@users.noreply.github.com>
2018-12-07 23:22:03 +0300

    my site

---
+++ CoffeeBook/js/javascript.js
@@ -0,0 +1,97 @@
+function poLin() {
+                        $('.coffee-animate').animate({
+                            left: '160px',
+                            top: '150px'
+                        }, 2000);
+                        
+                         $('.milk-animate').animate({
+                           
+                            top: '100px'
+                        }, 2000);
+                        
+                         $('.kori-animate').animate({
+                            right: '180px',
+                            top: '180px'
+                        }, 2000);
+                        
+                        $('.sweet').animate({
+                            left:'140px'                            
+                        }, 2000);
+                        
+                        $('.tea').animate({
+                            right: '140px'
+                        
+                        }, 2000);
+                    }
+
+var elements1 = $('.modal-overlay-1, .modal-1');
+$('.book-1').click(function(){
+    elements1.addClass('active');
+});
+$('.close-modal-1').click(function(){
+    elements1.removeClass('active');
+});
+
+var elements2 = $('.modal-overlay-2, .modal-2');
+$('.book-2').click(function(){
+    elements2.addClass('active');
+});
+$('.close-modal-2').click(function(){
+    elements2.removeClass('active');
+});
+
+var elements3 = $('.modal-overlay-3, .modal-3');
+$('.book-3').click(function(){
+    elements3.addClass('active');
+});
+$('.close-modal-3').click(function(){
+    elements3.removeClass('active');
+});
+
+var elements4 = $('.modal-overlay-4, .modal-4');
+$('.book-4').click(function(){
+    elements4.addClass('active');
+});
+$('.close-modal-4').click(function(){
+    elements4.removeClass('active');
+});
+
+var elements5 = $('.modal-overlay-5, .modal-5');
+$('.book-5').click(function(){
+    elements5.addClass('active');
+});
+$('.close-modal-5').click(function(){
+    elements5.removeClass('active');
+});
+
+var elements6 = $('.modal-overlay-6, .modal-6');
+$('.book-6').click(function(){
+    elements6.addClass('active');
+});
+$('.close-modal-6').click(function(){
+    elements6.removeClass('active');
+});
+
+var elements7 = $('.modal-overlay-7, .modal-7');
+$('.book-7').click(function(){
+    elements7.addClass('active');
+});
+$('.close-modal-7').click(function(){
+    elements7.removeClass('active');
+});
+
+var elements8 = $('.modal-overlay-8, .modal-8');
+$('.book-8').click(function(){
+    elements8.addClass('active');
+});
+$('.close-modal-8').click(function(){
+    elements8.removeClass('active');
+});
+
+var elements9 = $('.modal-overlay-9, .modal-9');
+$('.book-9').click(function(){
+    elements9.addClass('active');
+});
+$('.close-modal-9').click(function(){
+    elements9.removeClass('active');
+});
